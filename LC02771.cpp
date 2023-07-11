4f156dbb6783ce52da6b5822c5aa77c6c4057a67
Srihari <114772388+noob-coder211@users.noreply.github.com>
2023-07-10 17:27:14 -0700

    Create LC02771.cpp

---
+++ LC02771.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int maxNonDecreasingLength(vector<int>& nums1, vector<int>& nums2) {
+        int n = nums1.size();
+        vector<vector<int>> dp(n, vector<int>(2,1));
+        int ans = 1;
+        if(n == 1)
+            return ans;
+
+        for(int i=1;i<n;i++)
+        {
+            if(nums1[i] >= nums1[i-1])
+                dp[i][0] = 1 + dp[i-1][0];
+            if(nums1[i] >= nums2[i-1])
+                dp[i][0] = max(dp[i][0],  1+ dp[i-1][1]);
+            if(nums2[i] >= nums2[i-1])
+                dp[i][1] = 1+ dp[i-1][1];
+            if(nums2[i] >= nums1[i-1])
+                dp[i][1] = max(dp[i][1], 1+ dp[i-1][0]);
+
+            ans = max(ans, max(dp[i][0], dp[i][1]));
+        }
+        return ans;
+
+        
+        
+    }
+};
