9d521cc34c0a43de702e2c680c69e2abd2e5b6f9
Srihari <114772388+noob-coder211@users.noreply.github.com>
2023-07-17 16:26:04 -0700

    Create LC00703.cpp

---
+++ LC00703.cpp
@@ -0,0 +1,37 @@
+class KthLargest {
+public:
+
+    priority_queue<int, vector<int> , greater<int>> pq;
+    int n ;
+
+    KthLargest(int k, vector<int>& nums) {
+
+        n = k;
+
+        for(int num: nums)
+            add(num);  
+    }
+    
+    int add(int val) {
+
+        if(pq.size() < n)
+            pq.push(val);
+        else
+        {
+            if(pq.top() < val)
+            {
+                pq.pop();
+                pq.push(val);
+            }
+        }
+
+        return pq.top();
+        
+    }
+};
+
+/**
+ * Your KthLargest object will be instantiated and called as such:
+ * KthLargest* obj = new KthLargest(k, nums);
+ * int param_1 = obj->add(val);
+ */
