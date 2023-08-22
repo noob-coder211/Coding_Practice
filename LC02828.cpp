d53f21d3972d4572701dad41a47846654f4f34a9
Srihari <114772388+noob-coder211@users.noreply.github.com>
2023-08-21 17:56:41 -0700

    Create LC02828.cpp

---
+++ LC02828.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool isAcronym(vector<string>& words, string s) {
+        string ans = "";
+        for(string word: words)
+        {
+            ans+= word[0];
+        }
+        return ans == s;
+        
+    }
+};
